07489f9cdf7c554dd9060ab44e061fe186c08277
Naveen Narayan <naveen.narayan@salesforce.com>
2024-04-05 18:26:42 -0700

    insertion sort

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -2,6 +2,13 @@
 Binary Search: Given a sorted array and a target, find if the target exists in the array. If it does, return the index of the target within the array. If not, return -1.
 [Assume the array doesn't have any duplicates.]
 
+    Algorithm:
+        Step 1 − Select the middle item in the array and compare it with the key value to be searched. If it is matched, return the position of the median.
+        Step 2 − If it does not match the key value, check if the key value is either greater than or less than the median value.
+        Step 3 − If the key is greater, perform the search in the right sub-array; but if the key is lower than the median value, perform the search in the left sub-array.
+        Step 4 − Repeat Steps 1, 2 and 3 iteratively, until the size of sub-array becomes 1.
+        Step 5 − If the key value does not exist in the array, then the algorithm returns an unsuccessful search.
+
     Pseudo code:
         left = 0
         right = len(arr) - 1
